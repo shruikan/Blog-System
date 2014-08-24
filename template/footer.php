bc681e5cda05265b21cb10abd1bb888d49b9f789
Anton Cholakov <anton_cholakov@ymail.com>
2014-08-24 13:24:31 +0300

    Added Socials icons, changed some styles

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,21 @@
 </div>
 <hr />
 <footer>
-    <p class="text-center">&copy; <?php echo $site_title ?> 2014. It's all free..</p>
+	<div class="container">
+		<p class="pull-left">&copy; <?php echo $site_title ?> 2014. It's all free..</p>
+		<p class="pull-right socials">
+			Follow Us: 
+			<a href="#" class="btn btn-social btn-facebook">
+				<i class="fa fa-facebook"></i>
+			</a>
+			<a href="#" class="btn btn-social btn-twitter">
+				<i class="fa fa-twitter"></i>
+			</a>
+			<a href="#" class="btn btn-social btn-linkedin">
+				<i class="fa fa-linkedin"></i>
+			</a>
+		</p>
+	</div>
 </footer>
 
 <?php
