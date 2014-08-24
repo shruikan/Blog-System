b2bbd0fc7d1cfb1e7f50e9167aab407dd8ab2914
d-donchev <ddonchev@gmail.com>
2014-08-25 00:09:20 +0300

    About with pict

--- views/about.php
+++ views/about.php
@@ -6,7 +6,7 @@
     <div class="col-md-4">
         <div class="thumbnail">
             <a href="#">
-                <img src="http://placehold.it/180x180" class="img-responsive" />
+                <img src="https://softuni.bg/Users/Profile/ShowAvatar/f40f0b81-be96-493a-9cfd-ae9e15c3e3a6" class="img-responsive" />
             </a>
             <div class="caption text-center">
                 <h4>Name</h4>
@@ -20,7 +20,7 @@
     <div class="col-md-4">
         <div class="thumbnail">
             <a href="#">
-                <img src="http://placehold.it/180x180" class="img-responsive" />
+                <img src="https://softuni.bg/Users/Profile/ShowAvatar/477f2fa2-867f-4e57-b4c5-6d3c1c18eccd" class="img-responsive" />
             </a>
             <div class="caption text-center">
                 <h4>Name</h4>
@@ -34,7 +34,7 @@
     <div class="col-md-4">
         <div class="thumbnail">
             <a href="#">
-                <img src="http://placehold.it/180x180" class="img-responsive" />
+                <img src="https://softuni.bg/Users/Profile/ShowAvatar/a7af5957-756f-4545-a8fa-1c858af6ed70" class="img-responsive" />
             </a>
             <div class="caption text-center">
                 <h4>Name</h4>
@@ -48,7 +48,7 @@
     <div class="col-md-4">
         <div class="thumbnail">
             <a href="#">
-                <img src="http://placehold.it/180x180" class="img-responsive" />
+                <img src="https://softuni.bg/Users/Profile/ShowAvatar/b7b69109-2893-4d88-9d53-39feb38bb8b5" class="img-responsive" />
             </a>
             <div class="caption text-center">
                 <h4>Name</h4>
@@ -62,7 +62,7 @@
     <div class="col-md-4">
         <div class="thumbnail">
             <a href="#">
-                <img src="http://placehold.it/270x150" class="img-responsive" />
+                <img src="https://softuni.bg/Users/Profile/ShowAvatar/ac368eb3-ea21-4ad2-a1b2-cfd90675e934" class="img-responsive" />
             </a>
             <div class="caption text-center">
                 <h4>Name</h4>
@@ -76,7 +76,7 @@
     <div class="col-md-4">
         <div class="thumbnail">
             <a href="#">
-                <img src="http://placehold.it/270x150" class="img-responsive" />
+                <img src="https://softuni.bg/Users/Profile/ShowAvatar/ca3f843c-019b-4f99-9bd9-d63c500bac54" class="img-responsive" />
             </a>
             <div class="caption text-center">
                 <h4>Name</h4>
